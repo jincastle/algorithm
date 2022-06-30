f440d7f75ba50762ca64d2b6859db51211962571
jinsung Park <jinsungpark@jinsungui-MacBookPro.local>
2022-06-30 20:54:47 +0900

    leetcode

---
+++ python/palindrome_number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x = list(str(x))
+        return x == x[::-1]
+                
