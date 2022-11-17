7422f0bf5c1c0d37ffb1d839ab3cbab520b3dee1
jinsung Park <jinsungpark@jinsungui-MacBookPro.local>
2022-11-17 17:19:05 +0900

    코딩 테스트 study

--- python/grid_challenge.py
+++ python/grid_challenge.py
@@ -6,9 +6,6 @@ def gridChallenge(grid):
     for row in grid:
         newgrid.append(sorted(row))
 
-        # for row in newgrid:
-        #    print(row)
-
         for ind in range(len(grid)):
             for jnd in range(ind, len(grid[0])):
                 newgrid[ind][jnd], newgrid[jnd][ind] = newgrid[jnd][ind], newgrid[ind][jnd]
@@ -19,3 +16,19 @@ def gridChallenge(grid):
                         break
 
                     return res
+
+
+#좀더 좋은 코드
+def gridChallenge(grid):
+    # Write your code here
+    grid =[list(row) for row in grid]
+    r = len(grid)
+    c = len(grid[0])
+    for i in range(r):
+        grid[i].sort()
+        
+    for j in range(c):
+        for i in range(1,r):
+            if not grid[i-1][j] <= grid[i][j]:
+                return "NO"
+    return "YES"
